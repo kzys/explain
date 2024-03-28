13a8c968a8cbd95666c05504c0686e2edecc9def
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2024-03-27 22:44:06 -0700

    Take a title from Markdown

--- gen/src/page.rs
+++ gen/src/page.rs
@@ -1,4 +1,4 @@
-use pulldown_cmark::{html, Parser};
+use pulldown_cmark::{html, Event, HeadingLevel, Parser, Tag, TagEnd};
 use std::fs::File;
 use std::io::{BufReader, Read};
 use std::path::Path;
@@ -6,12 +6,15 @@ use yaml_rust::{yaml::Yaml, YamlLoader};
 
 pub struct Page {
     front_matter: Vec<Yaml>,
+    title_from_md: Option<String>,
     pub body_html: String,
 }
 
 impl Page {
     pub fn title(&self) -> Option<&str> {
-        self.front_matter[0]["title"].as_str()
+        self.front_matter[0]["title"]
+            .as_str()
+            .or(self.title_from_md.as_ref().map(|s| s.as_str()))
     }
 
     pub fn from_str(content: &str) -> Page {
@@ -26,9 +29,30 @@ impl Page {
         };
 
         let parser = Parser::new(md);
+        let events: Vec<Event> = parser.collect();
+
+        let mut heading_level: Option<HeadingLevel> = None;
+        let mut title_from_md: Option<String> = None;
+
+        for ev in events.clone() {
+            match ev {
+                Event::Start(Tag::Heading { level: lv, .. }) => {
+                    heading_level = Some(lv);
+                }
+                Event::End(TagEnd::Heading(..)) => {
+                    heading_level = None;
+                }
+                Event::Text(s) if heading_level != None => {
+                    title_from_md = Some(s.to_string());
+                }
+                _ => {}
+            }
+        }
+
         let mut html_output = String::new();
-        html::push_html(&mut html_output, parser);
+        html::push_html(&mut html_output, events.into_iter());
         return Page {
+            title_from_md,
             front_matter,
             body_html: html_output,
         };
@@ -57,4 +81,11 @@ fn test_hello() {
     let p = Page::from_str(&s);
     assert_eq!(p.title(), Some("hello"));
     assert_eq!(p.body_html, "<p>hello world</p>\n");
+}
+
+#[test]
+fn test_markdown_only() {
+    let p = Page::from_str("# hello");
+    assert_eq!(p.title(), Some("hello"));
+    assert_eq!(p.body_html, "<h1>hello</h1>\n");
 }
