7f1dd38b6bb2cf3a3f1a13f7888150f2c255b725
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2024-08-16 00:56:38 -0700

    fix: don't use aside
    but I can't recall why.

--- sidenote.ts
+++ sidenote.ts
@@ -1,37 +1,47 @@
 import {Node, FootnoteDefinition, FootnoteReference, Paragraph} from 'mdast';
 import {VFile} from 'vfile';
-import {visit, CONTINUE} from 'unist-util-visit'
+import {visit, CONTINUE, SKIP} from 'unist-util-visit'
 
 export function remarkSidenote() {
     function f(tree: Node, file: VFile) {
         let defs = {}
-        visit(tree, ['paragraph', 'footnoteDefinition'], (node: Node, index: number|undefined, parent: any) => {
-            if (node.type == 'paragraph') {
-                let p = node as Paragraph;
-                let c = p.children.find(c => c.type == 'footnoteReference')
-                if (c) {
-                    let ref = c as FootnoteReference;
-                    let def = defs[ref.identifier] as FootnoteDefinition;
-                    console.log(p.children)
-                    parent.children = [
-                        ...parent.children.slice(0, index),
-                        p,
-                        {
-                            type: 'paragraph',
-                            children: [
-                                {type: 'html', value: '<aside>'},
-                                ...def.children,
-                                {type: 'html', value: '</aside>'},
-                            ]},
-                        ...parent.children.slice(index!+1),
-                    ]
-                }
-            } else if (node.type == 'footnoteDefinition') {
-                let def = node as FootnoteDefinition;
-                defs[def.identifier] = def;
+        visit(tree, ['footnoteDefinition'], (node: Node, index: number|undefined, parent: any) => {
+            if (node.type != 'footnoteDefinition') {
+                return CONTINUE;
             }
+            let def = node as FootnoteDefinition;
+            defs[def.identifier] = def;
+
+            parent.children = [
+                ...parent.children.slice(0, index),
+                ...parent.children.slice(index!+1),
+            ]
+            return SKIP;
+         })
+
+         let fnIndex = 1;
+         visit(tree, ['footnoteReference'], (node: Node, index: number|undefined, parent: any) => {
+            if (node.type != 'footnoteReference') {
+                return CONTINUE;
+            }
+            let ref = node as FootnoteReference;
+            let def = defs[ref.identifier] as FootnoteDefinition;
+            console.log(def)
+            let id = `user-content-fn-${def.identifier}`
+            parent.children = [
+                ...parent.children.slice(0, index),
+                ref,
+                {
+                    type: 'html',
+                    value: `<small class=sidenote id=${id}>${fnIndex} `,
+                },
+                ...(def.children[0] as Paragraph).children,
+                {type: 'html', value: '</small>'},
+                ...parent.children.slice(index!+1),
+            ]
+            fnIndex++;
             return CONTINUE;
-         }, true)
+         })
       }
     return f;
 }
