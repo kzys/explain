f54a85e08a8b2bbcdc0cee852b233127f20ac88f
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2025-05-06 22:47:49 -0700

    test: test run

--- gen.rb
+++ gen.rb
@@ -21,21 +21,12 @@ class Page
   attr_accessor :url
   attr_accessor :draft
   attr_accessor :ctime, :mtime
-    
-  
-  def self.find(pattern)
-    g = Gen.new
-    Dir.glob(pattern).map do |path|
-      g.parse_file(path)
-    end.delete_if do |page|
-      page and page.draft
-    end.sort_by {|p| p.ctime.to_s }.reverse
-  end
 end
 
 class Gen
-  def initialize
-    @src_dir = Pathname('src')
+  def initialize(src_dir, dest_dir)
+    @src_dir = Pathname(src_dir)
+    @dest_dir = Pathname(dest_dir)
 
     stdin, stdout, stderr, wait_thr = Open3.popen3('git', 'log', '--name-only', "--format=format:\t%aI")
     stdin.close
@@ -95,17 +86,14 @@ class Gen
   end
   
   def run
-    src_dir = Pathname('src')
-    public_dir = Pathname('public')
-
     layout = ERB.new(Pathname('view').join('layout.html.erb').read)
 
-    Find.find('src') do |path|
+    Find.find(@src_dir.to_s) do |path|
       next if File.directory?(path)
       next if path =~ /~$/
 
       path = Pathname(path)
-      html_path = public_dir + path.relative_path_from(src_dir)
+      html_path = @dest_dir + path.relative_path_from(@src_dir)
 
       d = html_path.dirname
       d.mkpath unless d.exist?
@@ -122,7 +110,7 @@ class Gen
           end
         end
       when '.erb'
-        dest = public_dir + path.relative_path_from(src_dir)
+        dest = @dest_dir + path.relative_path_from(@src_dir)
 
         File.open(dest.to_s.gsub(/\.erb$/, ''), 'w') do |f|
           f.write(ERB.new(path.read).result(binding))
@@ -134,6 +122,14 @@ class Gen
       end
     end
   end
+
+  def find(pattern)
+    Dir.glob(pattern).map do |path|
+      parse_file(path)
+    end.delete_if do |page|
+      page and page.draft
+    end.sort_by {|p| p.ctime.to_s }.reverse
+  end
 end
 
-Gen.new.run if __FILE__ == $0
+Gen.new('src', 'public').run if __FILE__ == $0
--- test/test_gen.rb
+++ test/test_gen.rb
@@ -6,17 +6,20 @@ require 'minitest/autorun'
 require_relative '../gen'
 
 class TestGen < Minitest::Test
+  def setup
+    dir = Dir.mktmpdir
+    @gen = Gen.new(Pathname('testdata/src'), dir)
+  end
+
   def test_front_matter
-    g = Gen.new
-    vm = g.parse_file('test/front_matter.md')
+    vm = @gen.parse_file('test/front_matter.md')
     assert_equal('from front matter', vm.title)
     assert_match(/hello world/, vm.content)
     assert_equal(true, vm.draft)
   end
 
   def test_markdown
-    g = Gen.new
-    vm = g.parse_file('test/markdown.md')
+    vm = @gen.parse_file('test/markdown.md')
     assert_equal('h1', vm.title)
     assert_equal(<<END, vm.content)
 <h1>h1</h1>
@@ -28,9 +31,12 @@ END
   end
 
   def test_parse_git_log
-    g = Gen.new
     log = File.read('testdata/git_log.txt')
-    file_to_time = g.parse_git_log(log)
+    file_to_time = @gen.parse_git_log(log)
     assert(file_to_time)
+  end
+
+  def test_run
+    @gen.run
   end
 end
